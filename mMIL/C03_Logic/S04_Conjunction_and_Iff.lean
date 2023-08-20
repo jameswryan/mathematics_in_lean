de2e2320bd6c70657e814f8864080b3e6e7af5eb
James Ryan <james@wryan.dev>
2023-08-20 08:06:48 -0500

    mMIL

---
+++ mMIL/C03_Logic/S04_Conjunction_and_Iff.lean
@@ -0,0 +1,202 @@
+import Mathlib.Tactic
+import Mathlib.Data.Real.Basic
+import Mathlib.Data.Nat.Prime
+
+namespace C03S04
+
+example {x y : ℝ} (h₀ : x ≤ y) (h₁ : ¬y ≤ x) : x ≤ y ∧ x ≠ y := by
+  constructor
+  · assumption
+  intro h
+  apply h₁
+  rw [h]
+
+example {x y : ℝ} (h₀ : x ≤ y) (h₁ : ¬y ≤ x) : x ≤ y ∧ x ≠ y :=
+  ⟨h₀, fun h ↦ h₁ (by rw [h])⟩
+
+example {x y : ℝ} (h₀ : x ≤ y) (h₁ : ¬y ≤ x) : x ≤ y ∧ x ≠ y :=
+  have h : x ≠ y := by
+    contrapose! h₁
+    rw [h₁]
+  ⟨h₀, h⟩
+
+example {x y : ℝ} (h : x ≤ y ∧ x ≠ y) : ¬y ≤ x := by
+  rcases h with ⟨h₀, h₁⟩
+  contrapose! h₁
+  exact le_antisymm h₀ h₁
+
+example {x y : ℝ} : x ≤ y ∧ x ≠ y → ¬y ≤ x := by
+  rintro ⟨h₀, h₁⟩ h'
+  exact h₁ (le_antisymm h₀ h')
+
+example {x y : ℝ} : x ≤ y ∧ x ≠ y → ¬y ≤ x :=
+  fun ⟨h₀, h₁⟩ h' ↦ h₁ (le_antisymm h₀ h')
+
+example {x y : ℝ} (h : x ≤ y ∧ x ≠ y) : ¬y ≤ x := by
+  have ⟨h₀, h₁⟩ := h
+  contrapose! h₁
+  exact le_antisymm h₀ h₁
+
+example {x y : ℝ} (h : x ≤ y ∧ x ≠ y) : ¬y ≤ x := by
+  cases h
+  case intro h₀ h₁ =>
+    contrapose! h₁
+    exact le_antisymm h₀ h₁
+
+example {x y : ℝ} (h : x ≤ y ∧ x ≠ y) : ¬y ≤ x := by
+  cases h
+  next h₀ h₁ =>
+    contrapose! h₁
+    exact le_antisymm h₀ h₁
+
+example {x y : ℝ} (h : x ≤ y ∧ x ≠ y) : ¬y ≤ x := by
+  match h with
+    | ⟨h₀, h₁⟩ =>
+        contrapose! h₁
+        exact le_antisymm h₀ h₁
+
+example {x y : ℝ} (h : x ≤ y ∧ x ≠ y) : ¬y ≤ x := by
+  intro h'
+  apply h.right
+  exact le_antisymm h.left h'
+
+example {x y : ℝ} (h : x ≤ y ∧ x ≠ y) : ¬y ≤ x :=
+  fun h' ↦ h.right (le_antisymm h.left h')
+
+example {m n : ℕ} (h : m ∣ n ∧ m ≠ n) : m ∣ n ∧ ¬n ∣ m := by
+  have ⟨h₀, h₁⟩ := h
+  constructor
+  . apply h₀
+  intro h'
+  rw [ne_comm, ne_eq] at h₁
+  apply absurd (dvd_antisymm h' h₀) h₁
+
+example : ∃ x : ℝ, 2 < x ∧ x < 4 :=
+  ⟨5 / 2, by norm_num, by norm_num⟩
+
+example (x y : ℝ) : (∃ z : ℝ, x < z ∧ z < y) → x < y := by
+  rintro ⟨z, xltz, zlty⟩
+  exact lt_trans xltz zlty
+
+example (x y : ℝ) : (∃ z : ℝ, x < z ∧ z < y) → x < y :=
+  fun ⟨z, xltz, zlty⟩ ↦ lt_trans xltz zlty
+
+example : ∃ x : ℝ, 2 < x ∧ x < 4 := by
+  use 5 / 2
+  constructor <;> norm_num
+
+example : ∃ m n : ℕ, 4 < m ∧ m < n ∧ n < 10 ∧ Nat.Prime m ∧ Nat.Prime n := by
+  use 5
+  use 7
+  norm_num
+
+example {x y : ℝ} : x ≤ y ∧ x ≠ y → x ≤ y ∧ ¬y ≤ x := by
+  rintro ⟨h₀, h₁⟩
+  use h₀
+  exact fun h' ↦ h₁ (le_antisymm h₀ h')
+
+example {x y : ℝ} (h : x ≤ y) : ¬y ≤ x ↔ x ≠ y := by
+  constructor
+  · contrapose!
+    rintro rfl
+    rfl
+  contrapose!
+  exact le_antisymm h
+
+example {x y : ℝ} (h : x ≤ y) : ¬y ≤ x ↔ x ≠ y :=
+  ⟨fun h₀ h₁ ↦ h₀ (by rw [h₁]), fun h₀ h₁ ↦ h₀ (le_antisymm h h₁)⟩
+
+example {x y : ℝ} : x ≤ y ∧ ¬y ≤ x ↔ x ≤ y ∧ x ≠ y := by
+  constructor
+  . intro h
+    have ⟨h₀, h₁⟩ := h
+    constructor
+    . apply h₀
+    . apply Ne.symm (ne_of_not_le h₁)
+  . intro h
+    have ⟨h₀, h₁⟩ := h
+    constructor
+    . apply h₀
+    . have xly : x < y := lt_of_le_of_ne h₀ h₁
+      apply Iff.mpr not_le xly
+
+
+
+theorem aux {x y : ℝ} (h : x ^ 2 + y ^ 2 = 0) : x = 0 :=
+  have h' : x ^ 2 = 0 := by linarith [pow_two_nonneg x, pow_two_nonneg y]
+  pow_eq_zero h'
+
+example (x y : ℝ) : x ^ 2 + y ^ 2 = 0 ↔ x = 0 ∧ y = 0 := by
+  constructor
+  . intro h
+    constructor
+    . exact aux h
+    . rw [add_comm] at h
+      exact aux h
+  rintro ⟨rfl, rfl⟩
+  ring
+
+section
+
+example (x : ℝ) : |x + 3| < 5 → -8 < x ∧ x < 2 := by
+  rw [abs_lt]
+  intro h
+  constructor <;> linarith
+
+example : 3 ∣ Nat.gcd 6 15 := by
+  rw [Nat.dvd_gcd_iff]
+  constructor <;> norm_num
+
+end
+
+theorem not_monotone_iff {f : ℝ → ℝ} : ¬Monotone f ↔ ∃ x y, x ≤ y ∧ f x > f y := by
+  rw [Monotone]
+  push_neg
+  rfl
+
+example : ¬Monotone fun x : ℝ ↦ -x := by
+  rw [not_monotone_iff]
+  use 0,1
+  norm_num
+
+section
+variable {α : Type _} [PartialOrder α]
+variable (a b : α)
+
+example : a < b ↔ a ≤ b ∧ a ≠ b := by
+  rw [lt_iff_le_not_le]
+  constructor
+  . intro h
+    have ⟨h₀, h₁⟩ := h
+    constructor
+    . apply h₀
+    . apply Ne.symm (ne_of_not_le h₁)
+  . intro h
+    have ⟨h₀, h₁⟩ := h
+    constructor
+    . apply h₀
+    . have alb : a < b := lt_of_le_of_ne h₀ h₁
+      apply not_le_of_gt alb
+
+end
+
+section
+variable {α : Type _} [Preorder α]
+variable (a b c : α)
+
+example : ¬a < a := by
+  rw [lt_iff_le_not_le]
+  rintro ⟨alea, nalea⟩
+  exact nalea alea
+
+
+example : a < b → b < c → a < c := by
+  simp only [lt_iff_le_not_le]
+  rintro ⟨aleb, nblea⟩ ⟨blec, ncleb⟩
+  constructor
+  apply le_trans aleb blec
+  intro nclea
+  apply nblea
+  apply le_trans blec nclea
+
+end
